5d023ac11e920069b53590d724b661d80dd1c1e8
Michal Tyburski <Michal_Tyburski@Jabil.com>
2020-01-21 17:58:44 +0100

    ready to work

--- src/app/core/app.module.ts
+++ src/app/core/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { TopMenuComponent } from './components/menu/top-menu/top-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule,
